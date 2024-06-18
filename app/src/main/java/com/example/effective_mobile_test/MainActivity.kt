d01a91fa44179390fa53a6161b8689aaf532b8f8
AlexandrKorytin <al.corytin@yandex.ru>
2024-06-19 00:28:46 +0300

    начат код активити

--- app/src/main/java/com/example/effective_mobile_test/MainActivity.kt
+++ app/src/main/java/com/example/effective_mobile_test/MainActivity.kt
@@ -5,12 +5,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import com.example.effective_mobile_test.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-       
+        setContentView(binding.root)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.root_container_view) as NavHostFragment
+        val navController = navHostFragment.navController
     }
 }
