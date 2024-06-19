7bbfee0bffcabcd2e68ffc8ba585c446ff9b2584
AlexandrKorytin <al.corytin@yandex.ru>
2024-06-20 00:35:54 +0300

    написал класс результата запроса для домаин

---
+++ tickets/src/main/java/com/example/tickets/domain/models/SearchDataResult.kt
@@ -0,0 +1,11 @@
+package com.example.tickets.domain.models
+
+import androidx.annotation.StringRes
+
+
+sealed interface SearchResultData<T> {
+    data class Data<T>(val value: T?) : SearchResultData<T>
+    data class ErrorServer<T>(@StringRes val message: Int) : SearchResultData<T>
+    data class NoInternet<T>(@StringRes val message: Int) : SearchResultData<T>
+    data class Empty<T>(@StringRes val message: Int) : SearchResultData<T>
+}
