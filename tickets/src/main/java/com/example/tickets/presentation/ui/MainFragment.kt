ddecf37dfcce181240c015febb6b8a5288b5b726
AlexandrKorytin <al.corytin@yandex.ru>
2024-06-20 00:37:21 +0300

    начал главный фрагмент

--- tickets/src/main/java/com/example/tickets/presentation/ui/MainFragment.kt
+++ tickets/src/main/java/com/example/tickets/presentation/ui/MainFragment.kt
@@ -1,16 +1,29 @@
 package com.example.tickets.presentation.ui
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import com.example.tickets.databinding.FragmentMainBinding
 import com.example.util.BindingFragment
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainFragment : BindingFragment<FragmentMainBinding>() {
+
 
     override fun createBinding(
         inflater: LayoutInflater, container: ViewGroup?
     ): FragmentMainBinding {
         return FragmentMainBinding.inflate(inflater, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        bind()
+    }
+
+    private fun bind() {
+
     }
 
     companion object
