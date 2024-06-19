949e212c242aad33d1cac529dcc05683a4bc4e12
AlexandrKorytin <al.corytin@yandex.ru>
2024-06-19 13:43:50 +0300

    добавил фрагмент заглушку

---
+++ tickets/src/main/java/com/example/tickets/MainFragment.kt
@@ -0,0 +1,17 @@
+package com.example.tickets
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.example.tickets.databinding.FragmentMainBinding
+import com.example.util.BindingFragment
+
+class MainFragment : BindingFragment<FragmentMainBinding>() {
+
+    override fun createBinding(
+        inflater: LayoutInflater, container: ViewGroup?
+    ): FragmentMainBinding {
+        return FragmentMainBinding.inflate(inflater, container, false)
+    }
+
+    companion object
+}
