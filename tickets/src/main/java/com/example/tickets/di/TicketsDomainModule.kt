b079cec2a353487063456adf9e3fda25979ca859
AlexandrKorytin <al.corytin@yandex.ru>
2024-06-20 14:40:08 +0300

    - добавил зависимости

---
+++ tickets/src/main/java/com/example/tickets/di/TicketsDomainModule.kt
@@ -0,0 +1,37 @@
+package com.example.tickets.di
+
+import com.example.tickets.data.repository.impl.AllTicketsRepositoryImpl
+import com.example.tickets.data.repository.impl.CheckedTicketsRepositoryImpl
+import com.example.tickets.data.repository.impl.MainRepositoryImpl
+import com.example.tickets.domain.api.interacrors.AllTicketsInteractor
+import com.example.tickets.domain.api.interacrors.CheckedTicketsInteractor
+import com.example.tickets.domain.api.interacrors.MainInteractor
+import com.example.tickets.domain.api.repositories.AllTicketsRepository
+import com.example.tickets.domain.api.repositories.CheckedTicketsRepository
+import com.example.tickets.domain.api.repositories.MainRepository
+import com.example.tickets.domain.impl.AllTicketsInteractorImpl
+import com.example.tickets.domain.impl.CheckedTicketsInteractorImpl
+import com.example.tickets.domain.impl.MainInteractorImpl
+import org.koin.dsl.module
+
+val aminDomainModule = module {
+    single<MainRepository> {
+        MainRepositoryImpl(client = get())
+    }
+    single<CheckedTicketsRepository> {
+        CheckedTicketsRepositoryImpl(client = get())
+    }
+    single<AllTicketsRepository> {
+        AllTicketsRepositoryImpl(client = get())
+    }
+
+    factory<MainInteractor> {
+        MainInteractorImpl(repository = get())
+    }
+    factory<CheckedTicketsInteractor> {
+        CheckedTicketsInteractorImpl(repository = get())
+    }
+    factory<AllTicketsInteractor> {
+        AllTicketsInteractorImpl(repository = get())
+    }
+}
