8a6b3b8c29d4cea1f495b6601d0493ee57857a7d
AlexandrKorytin <al.corytin@yandex.ru>
2024-06-21 00:09:27 +0300

    добавил параметр город в ошибки и новый класс - загрузка

--- tickets/src/main/java/com/example/tickets/presentation/models/main/MainScreenState.kt
+++ tickets/src/main/java/com/example/tickets/presentation/models/main/MainScreenState.kt
@@ -5,7 +5,8 @@ import com.example.tickets.domain.models.OfferData
 import com.example.tickets.domain.models.Town
 
 sealed interface MainScreenState {
+    data class IsLoading(val town: Town): MainScreenState
     data class Content(val offers: List<OfferData>, val town: Town) : MainScreenState
-    data class Error(@StringRes val message: Int) : MainScreenState
-    data class NoInternet(@StringRes val message: Int) : MainScreenState
+    data class Error(@StringRes val message: Int, val town: Town) : MainScreenState
+    data class NoInternet(@StringRes val message: Int, val town: Town) : MainScreenState
 }
