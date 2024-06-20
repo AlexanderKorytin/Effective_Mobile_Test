58e54bec5cd71d5b606251a5dee659311ee64e99
AlexandrKorytin <al.corytin@yandex.ru>
2024-06-21 00:08:36 +0300

    поменял окончания запросов в соответсвии с изменениями в тз

--- network/src/main/java/com/example/network/MokApi.kt
+++ network/src/main/java/com/example/network/MokApi.kt
@@ -5,8 +5,8 @@ import com.example.data.models.search.RecommendTicketsDto
 import com.example.data.models.tickets.TicketsDto
 import retrofit2.http.GET
 
-private const val MAIN_END = "214a1713-bac0-4853-907c-a1dfc3cd05fd"
-private const val RECOMMEND_END = "7e55bf02-89ff-4847-9eb7-7d83ef884017"
+private const val MAIN_END = "ad9a46ba-276c-4a81-88a6-c068e51cce3a"
+private const val RECOMMEND_END = "38b5205d-1a3d-4c2f-9d77-2f9d1ef01a4a"
 private const val ALL_TICKETS_END = "c0464573-5a13-45c9-89f8-717436748b69"
 
 interface MokApi {
