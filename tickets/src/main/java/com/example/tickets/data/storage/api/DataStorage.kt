be00c81c87d24dcd75be2e92f0b17467f0125896
AlexandrKorytin <al.corytin@yandex.ru>
2024-06-23 23:08:06 +0300

    добавил функцию получения списка рекомендаций

--- tickets/src/main/java/com/example/tickets/data/storage/api/DataStorage.kt
+++ tickets/src/main/java/com/example/tickets/data/storage/api/DataStorage.kt
@@ -1,6 +1,9 @@
 package com.example.tickets.data.storage.api
 
+import com.example.tickets.data.models.RecommendationsItemDto
+
 interface DataStorage {
     fun setDepartureTown(town: String)
     fun getDepartureTown(): String
+    fun getRecommendationsList(): List<RecommendationsItemDto>
 }
--- tickets/src/main/java/com/example/tickets/data/storage/impl/SharedPrefStorage.kt
+++ tickets/src/main/java/com/example/tickets/data/storage/impl/SharedPrefStorage.kt
@@ -1,6 +1,8 @@
 package com.example.tickets.data.storage.impl
 
 import android.content.SharedPreferences
+import com.example.tickets.R
+import com.example.tickets.data.models.RecommendationsItemDto
 import com.example.tickets.data.storage.api.DataStorage
 
 class SharedPrefStorage(private val sharedPref: SharedPreferences) : DataStorage {
@@ -10,6 +12,27 @@ class SharedPrefStorage(private val sharedPref: SharedPreferences) : DataStorage
 
     override fun getDepartureTown(): String {
         return sharedPref.getString(TOWN, EMPTY_STR) ?: EMPTY_STR
+    }
+
+    override fun getRecommendationsList(): List<RecommendationsItemDto> {
+        return listOf(
+            RecommendationsItemDto(
+                icon = R.drawable.ic_istanbul,
+                destinationMane = R.string.istanbul,
+                label = R.string.popular_destination
+            ),
+            RecommendationsItemDto(
+                icon = R.drawable.ic_sochi,
+                destinationMane = R.string.sochi,
+                label = R.string.popular_destination
+            ),
+            RecommendationsItemDto(
+                icon = R.drawable.ic_phuket,
+                destinationMane = R.string.phuket,
+                label = R.string.popular_destination
+            ),
+
+            )
     }
 }
 
