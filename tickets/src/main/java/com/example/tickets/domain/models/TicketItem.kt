70c17ed6466018f5c75a2bc67dac8670b1252c03
AlexandrKorytin <al.corytin@yandex.ru>
2024-06-24 01:24:08 +0300

    ренэйминг

--- tickets/src/main/java/com/example/tickets/domain/models/TicketItem.kt
+++ tickets/src/main/java/com/example/tickets/domain/models/TicketItem.kt
@@ -3,9 +3,9 @@ package com.example.tickets.domain.models
 data class TicketItem(
     val badge: Badge,
     val price: String,
-    val departureDate: String,
+    val departureTime: String,
     val departureAirport: String,
-    val arrivalDate: String,
+    val arrivalTime: String,
     val arrivalAirport: String,
     val duration: String,
     val hasTransfer: Boolean
