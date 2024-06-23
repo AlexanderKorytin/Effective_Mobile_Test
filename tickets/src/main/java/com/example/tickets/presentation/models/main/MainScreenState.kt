913568737e024133a860682d5a6a36fafee6d8a0
AlexandrKorytin <al.corytin@yandex.ru>
2024-06-23 23:18:56 +0300

    исправил ошибку

--- tickets/src/main/java/com/example/tickets/presentation/models/main/MainScreenState.kt
+++ tickets/src/main/java/com/example/tickets/presentation/models/main/MainScreenState.kt
@@ -1,10 +1,12 @@
 package com.example.tickets.presentation.models.main
 
+import com.example.tickets.domain.models.RecommendItem
 import com.example.tickets.domain.models.Town
 
-data class MainScreeState(
+data class MainScreenState(
     val data: MainScreenStateResponse = MainScreenStateResponse.IsLoading,
-    val departureTown: Town = Town(EMPTY_NAME)
+    val departureTown: Town = Town(EMPTY_NAME),
+    val listRecommend: List<RecommendItem> = emptyList()
 )
 
 private const val EMPTY_NAME = ""
