3e82832091c04cf1a4fc9b9dc746ef28b48b978a
AlexandrKorytin <al.corytin@yandex.ru>
2024-06-23 23:14:08 +0300

    написал модельку билета

--- tickets/src/main/java/com/example/tickets/domain/models/RecommendTicket.kt
+++ tickets/src/main/java/com/example/tickets/domain/models/RecommendTicket.kt
@@ -3,6 +3,7 @@ package com.example.tickets.domain.models
 import com.example.data.models.Price
 
 data class RecommendTicket(
+    val iconId: Int = 0,
     val price: String,
     val timeRange: String,
     val title: String
