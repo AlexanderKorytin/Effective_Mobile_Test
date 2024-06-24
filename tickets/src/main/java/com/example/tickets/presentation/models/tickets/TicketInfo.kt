170b172e0bb12746da4d2a6b71951a7c54c19ddd
AlexandrKorytin <al.corytin@yandex.ru>
2024-06-24 11:00:57 +0300

    написал модель данных информации о маршруте

---
+++ tickets/src/main/java/com/example/tickets/presentation/models/tickets/TicketInfo.kt
@@ -0,0 +1,12 @@
+package com.example.tickets.presentation.models.tickets
+
+import java.io.Serializable
+
+data class TicketInfo(
+    val departureTown: String = EMPTY,
+    val arrivalTown: String = EMPTY,
+    val ticketSettings: String = EMPTY,
+    val dateDeparture: String = EMPTY
+): Serializable
+
+private const val EMPTY = ""
