0b55f6a36f50e126c8484b6ff03eac0cac6cea96
AlexandrKorytin <al.corytin@yandex.ru>
2024-06-24 11:06:39 +0300

    добавил зависимости и дополнил граф навигации

--- tickets/src/main/java/com/example/tickets/di/TicketsDataModule.kt
+++ tickets/src/main/java/com/example/tickets/di/TicketsDataModule.kt
@@ -2,10 +2,12 @@ package com.example.tickets.di
 
 import com.example.tickets.data.storage.api.DataStorage
 import com.example.tickets.data.storage.impl.SharedPrefStorage
+import com.google.gson.Gson
 import org.koin.dsl.module
 
 val ticketsDataModule = module {
     single<DataStorage> {
         SharedPrefStorage(sharedPref = get())
     }
+    single { Gson() }
 }
--- tickets/src/main/java/com/example/tickets/di/TicketsViewModelsModule.kt
+++ tickets/src/main/java/com/example/tickets/di/TicketsViewModelsModule.kt
@@ -1,7 +1,9 @@
 package com.example.tickets.di
 
+import com.example.tickets.presentation.models.tickets.TicketInfo
 import com.example.tickets.presentation.viewmodels.main.MainViewModel
 import com.example.tickets.presentation.viewmodels.search.SearchViewModel
+import com.example.tickets.presentation.viewmodels.tickets.AllTicketsViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
@@ -9,7 +11,10 @@ val ticketsViewModelModule = module {
     viewModel<MainViewModel> {
         MainViewModel(interactor = get())
     }
-    viewModel<SearchViewModel>{(departureName: String, arrivalName: String) ->
-        SearchViewModel(interactor = get(), departureName = get(), arrivalName = get())
+    viewModel<SearchViewModel> { (departureName: String, arrivalName: String) ->
+        SearchViewModel(interactor = get(), departureName = departureName, arrivalName = arrivalName)
+    }
+    viewModel<AllTicketsViewModel> { (ticketInfo: String) ->
+        AllTicketsViewModel(interactor = get(), json = get(), ticketInfo = ticketInfo)
     }
 }
